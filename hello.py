975cb07b9aa6be33784a872bff78235c37e928af
Ankit Mistry <ankit.25587@gmail.com>
2025-12-24 11:35:11 +0530

    Add function to add 3 integers

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
 print("hello")
+
+def add_three_integers(a, b, c):
+    """Function to add 3 integers"""
+    return a + b + c
+
+if __name__ == "__main__":
+    result = add_three_integers(5, 10, 15)
+    print(f"Sum of 5, 10, 15 is: {result}")
